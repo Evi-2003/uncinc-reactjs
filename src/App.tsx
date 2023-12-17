aa32f46a6da003150b5e2e3bdba9e2f71cdd1af6
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-12-18 00:50:32 +0100

    App in context zetten

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import "./app.css";
 import Header from "./components/header";
 import Login from "./routes/login";
 import Home from "./routes/home";
+import { Inloggen } from "./auth/auth";
 // import Dashboard from "./routes/dashboard"; -> Zodra ik authenticatie heb geregeld
 function App() {
   return (
     <React.StrictMode>
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
+      <Inloggen>
+        <Header />
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Router>
+      </Inloggen>
     </React.StrictMode>
   );
 }
