2845d1a477f5ccea8e5efa14837c005e0eaef7d5
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-12-18 03:12:37 +0100

    Redirect na inlog
    Zodra je bent ingelogd, wordt je doorverwezen naar het dashboard.

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react"; // Remove useState import
 import LoginForm from "../components/loginForm";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../auth/auth";
 
 export default function Login() {
+  const { loggedIn, redirectOnLogin, setRedirectOnLogin } = useAuth();
+
+  useEffect(() => {
+    if (redirectOnLogin) {
+      setRedirectOnLogin(false);
+    }
+  }, [redirectOnLogin, setRedirectOnLogin]);
+
+  if (loggedIn || redirectOnLogin) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
     <>
       <h1>Login</h1>
