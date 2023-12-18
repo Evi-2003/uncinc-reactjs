367be593d631375c81b9e7dd481e05264abab96c
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-12-18 03:01:45 +0100

    Dashboard route toegevoegd & State management
    Met Navigate sturen we de gebruiker terug naar de login pagina.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from "react";
+import { ReactElement } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import "./app.css";
 import Header from "./components/header";
 import Login from "./routes/login";
 import Home from "./routes/home";
-import { Inloggen } from "./auth/auth";
-// import Dashboard from "./routes/dashboard"; -> Zodra ik authenticatie heb geregeld
+import { AuthProvider, useAuth } from "./auth/auth";
+import Dashboard from "./routes/dashboard";
+
+type PrivateRouteProps = {
+  children: ReactElement;
+};
+// PrivateRoute controleert of de gebruiker is ingelogd.
+// Als dat niet het geval is, wordt de gebruiker naar de loginpagina gestuurd.
+const PrivateRoute = ({
+  children,
+}: PrivateRouteProps): React.ReactElement | null => {
+  const { loggedIn } = useAuth();
+  return loggedIn ? children : <Navigate to="/login" />;
+};
+
 function App() {
   return (
     <React.StrictMode>
-      <Inloggen>
+      <AuthProvider>
         <Header />
         <Router>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </Router>
-      </Inloggen>
+      </AuthProvider>
     </React.StrictMode>
   );
 }
